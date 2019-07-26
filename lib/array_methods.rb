e32cbe27e2462eb156a6fb2881cc56643e83d792
Aaron Blum <blumaa@gmail.com>
2019-07-26 20:02:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,9 +12,10 @@ def find_element_index(array, value_to_find)
   array.each_with_index do |element, index| 
       if element == value_to_find
         return index
+      
+      else
+        return nil
       end
-    else
-      return nil
     end
 end
 
