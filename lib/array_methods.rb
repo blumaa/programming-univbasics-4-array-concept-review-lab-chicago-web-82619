53e3448edd9cf7e9f451214758179bce53130d52
Aaron Blum <blumaa@gmail.com>
2019-07-26 20:07:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,6 +18,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  loop
+  if the value is higher than current max value replace max value
 end
 
 def find_min_value(array)
