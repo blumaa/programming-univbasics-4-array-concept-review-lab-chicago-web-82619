0c14b1f30c3d8d8219c6daabf1a73ad60b0e8d0e
Aaron Blum <blumaa@gmail.com>
2019-07-26 20:20:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -30,7 +30,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min_val = []
+  min_val = array[0]
   array.each_with_index do |element, index|
     if element < min_val
       min_val = element
